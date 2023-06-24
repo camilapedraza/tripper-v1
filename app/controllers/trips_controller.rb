c2773a9a0c02abc91ac8067c793f2ef085c4cba4
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2023-06-24 15:25:14 +0200

    Events Index: Hero

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,19 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    @trip = Trip.find(params[:id])
+    @trippers = group_trippers(@trip)
+  end
+
+  private
+
+  def group_trippers(trip)
+    trippers = []
+    trippers << trip.user
+    trip.collaborators.each { |collaborator| trippers << collaborator.user }
+    trippers
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'trips/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :trips, only: :index
+  resources :trips, only: %i[index show] do
+    resources :events, only: :index
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
