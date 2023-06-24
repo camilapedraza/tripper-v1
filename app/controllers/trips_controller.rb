91e1bf8d0f2601637bd33bfb27cff72982fa48a1
Gaetan Tripelon <tripelong@gmail.com>
2023-06-24 12:45:13 +0200

    Added possibility to create a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,37 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: %i[edit update]
+
   def index
     @trips = Trip.all
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name)
   end
 end
