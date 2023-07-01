675f0b67f26e12c22c2f80590e430c9254ad760c
Gaetan Tripelon <tripelong@gmail.com>
2023-07-01 18:36:53 +0200

    trying to understand how it could work, almost here

--- app/javascript/controllers/event_type_selection_controller.js
+++ app/javascript/controllers/event_type_selection_controller.js
@@ -4,7 +4,27 @@ export default class extends Controller {
   static targets = ["type"]
 
   connect() {
-    console.log(this.typeTarget.innerHTML)
+
+    const journey = document.getElementById("journey")
+
+    this.typeTarget.addEventListener("change", () => console.log(this.typeTarget.value))
+
+
+    if (this.typeTarget.addEventListener("input", () => this.typeTarget.value) == "Journey") {
+      document.classList.toggle("d-none")
+    }
+
+    // this.typeTarget.addEventListener("input", () => document.getElementById(this.typeTarget.value).classList.toggle("d-none"));
+
+    // if (this.typeTarget.addEventListener("input", () => document.getElementById(this.typeTarge.value).classList.toggle("d-none"))) {
+    // }
+  }
+  change() {
+    let select = document.querySelector('#selector');
+        let result = document.querySelector('#result');
+        select.addEventListener('input', function () {
+            result.textContent = this.typeTarget.value;
+        });
   }
     // if (journey == this.typeTarget.innerText) {
     //   journey.classList.toggle("d-none")
@@ -13,8 +33,4 @@ export default class extends Controller {
     // if (this.typeTarget.innerText == ["Journey", "Flight", "Train", "Bus", "Boat"]) {
     //   this.typeTarget.classList.toggle(".d-none")
     // }
-  myFunction() {
-    const selector = this.getElementById("selector");
-    this.getElementById("demo").value = selector.options[selector.selectedIndex].text;
-  }
 }
