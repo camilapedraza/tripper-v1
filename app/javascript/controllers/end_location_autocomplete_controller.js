43c17a9e55e08a504dcad41bdedf0d8c5efb636e
Gaetan Tripelon <tripelong@gmail.com>
2023-07-08 17:57:52 +0200

    renamed labels on event_form + removed location duplicates

--- app/javascript/controllers/end_location_autocomplete_controller.js
+++ app/javascript/controllers/end_location_autocomplete_controller.js
@@ -12,7 +12,9 @@ export default class extends Controller {
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address"
     })
-    this.geocoder.addTo(this.element)
+    if (!this.element.querySelector(".mapboxgl-ctrl-geocoder")) {
+      this.geocoder.addTo(this.element)
+    }
     this.geocoder.on("result", event => this.#setInputValue(event))
     this.geocoder.on("clear", () => this.#clearInputValue())
   }
