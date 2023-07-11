ecfe14727cc9777d6bc655f783ed56b38c6e9b97
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2023-07-12 01:57:45 +0200

    File upload and labeling feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_trip, only: %i[show new create edit update]
+  before_action :set_trip, only: %i[show new create edit]
 
   def index
   end
@@ -24,7 +24,30 @@ class EventsController < ApplicationController
     end
   end
 
+  # MISSING: DELETE FILE
+  def add_file
+    @event = Event.find(params[:event_id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    # fetch user's selected label for file
+    file_label = params[:file_label]
+    @event.update(event_params)
+    # update saved file with selected label in form
+    update_filename(file_label) if @event.files.attached?
+    redirect_to trip_event_path(@event.trip, @event)
+  end
+
   private
+
+  def update_filename(label)
+    # preserve original extension
+    extension = @event.files.last.filename.extension
+    # rename using user-selected label + original extension
+    filename = "#{label}.#{extension}"
+    @event.files.last.update(filename:)
+  end
 
   def set_trip
     @trip = Trip.find(params[:trip_id])
@@ -39,9 +62,11 @@ class EventsController < ApplicationController
                                   :end_location,
                                   :provider,
                                   :reservation_number,
+                                  :transport_number,
                                   :seat_number,
                                   :notes,
                                   :provider_phone,
-                                  :provider_email)
+                                  :provider_email,
+                                  :files)
   end
 end
